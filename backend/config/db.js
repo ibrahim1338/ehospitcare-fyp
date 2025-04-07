f0b9d3d4414368e1c14464bcebb561400c61e85c
M Shahroz Ali <122813273+shahroz514@users.noreply.github.com>
2025-04-07 11:42:43 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose')
 async function connectDB(){
     try {
         // Add your database URL here EXAMPLE
-        return await mongoose.connect('mongodb://hospitcare:root@fyp-shard-00-00.u0mrp.mongodb.net:27017,fyp-shard-00-01.u0mrp.mongodb.net:27017,fyp-shard-00-02.u0mrp.mongodb.net:27017/?replicaSet=atlas-l5pmrq-shard-0&ssl=true&authSource=admin')
+        return await mongoose.connect('mongodb://root:root@fyp-shard-00-00.u0mrp.mongodb.net:27017,fyp-shard-00-01.u0mrp.mongodb.net:27017,fyp-shard-00-02.u0mrp.mongodb.net:27017/?replicaSet=atlas-l5pmrq-shard-0&ssl=true&authSource=admin')
     } catch{
         console.log('Database Connection Error')
     }
